5afdac56aed3189dd6b88dee22f4a88fb532bc5d
Bill Ingram <waingram@gmail.com>
2018-03-19 22:18:40 -0400

    Add Mohammed's name and e-mail to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['code_embeddings'],
     url='',
     license='BSD-3-Clause',
-    author='Bill Ingram',
-    author_email='waingram@vt.edu',
+    author='Bill Ingram, Mohammed El-Arnaoty',
+    author_email='waingram@vt.edu, marnaoty@vt.edu',
     description=''
 )
