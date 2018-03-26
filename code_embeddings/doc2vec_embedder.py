ffde20f412cf6779c0fca37b3268222893651f82
Bill Ingram <waingram@gmail.com>
2018-03-26 15:40:19 -0400

    More experimenting with doc2vec
    So far, the results aren't very good 😖

--- code_embeddings/doc2vec_embedder.py
+++ code_embeddings/doc2vec_embedder.py
@@ -1,40 +1,53 @@
 from gensim.models.doc2vec import TaggedDocument
 from gensim.models.doc2vec import Doc2Vec
+import gensim.models.doc2vec
 from code_embeddings.utils import tokenize
 from pathlib import Path
+import multiprocessing
+from random import shuffle
 
 
 class Doc2vecEmbedder():
+    cores = multiprocessing.cpu_count()
+    alpha, min_alpha, passes = (0.025, 0.001, 20)
+    alpha_delta = (alpha - min_alpha) / passes
+    assert gensim.models.doc2vec.FAST_VERSION > -1, "This will be painfully slow otherwise"
 
     def __init__(self):
         self._code_directory = None
         self._models_directory = None
         self._tagged_docs = None
-        self._model = None
+        # TODO: make these parameters
+        self._model = Doc2Vec(dm=1, dm_concat=1, vector_size=25, window=5, negative=5, hs=0, min_count=2, epochs=500,
+                              workers=self.cores)
 
     def _process_files(self):
-        for programming_task in self._code_directory.glob('./*/*'):
-            if not programming_task.is_dir():
+        for programming_language in self._code_directory.glob('./Java'):
+            if not programming_language.is_dir():
                 continue
-            for code_fragment in programming_task.glob('./*'):
-                # print('Processing "{}"'.format(code_fragment))
-                with code_fragment.open() as f:
-                    tokens = tokenize(f.read())
-                yield TaggedDocument(tokens, [programming_task.name])
+            for programming_task in programming_language.glob('./*'):
+                if not programming_task.is_dir():
+                    continue
+                for code_fragment in programming_task.glob('./*'):
+                    # print('Processing "{}"'.format(code_fragment))
+                    with code_fragment.open() as f:
+                        tokens = tokenize(f.read())
+                    yield TaggedDocument(tokens, [programming_task.name, programming_language.name])
 
     def build_model(self, code_directory):
         self._code_directory = Path(code_directory)
         self._tagged_docs = list(self._process_files())
         print('Doc2Vec: building the model')
-        self._model = Doc2Vec(self._tagged_docs, vector_size=100, window=8, min_count=2,
-                              workers=7)  # TODO: make these parameters
+        self._model.build_vocab(self._tagged_docs)
 
     def train(self):
-        for epoch in range(10):
+        for epoch in range(self.passes):
+            doc_list = self._tagged_docs
+            shuffle(doc_list)  # Shuffling gets best results
             if epoch % 2 == 0:
                 print('Now training epoch {}'.format(epoch))
             self._model.train(self._tagged_docs, total_examples=self._model.corpus_count, epochs=self._model.epochs)
-            self._model.alpha -= 0.002  # decrease the learning rate
+            self._model.alpha -= self.min_alpha  # decrease the learning rate
             self._model.min_alpha = self._model.alpha  # fix the learning rate, no decay
 
     def save_model(self, models_directory):
--- code_embeddings/experiments/doc2vec.py
+++ code_embeddings/experiments/doc2vec.py
@@ -1,4 +1,5 @@
 from code_embeddings.doc2vec_embedder import Doc2vecEmbedder
+import numpy as np
 
 code_dir = '../../data'
 models_dir = '../../models'
@@ -10,10 +11,11 @@ doc2vec.save_model(models_dir)
 
 model = doc2vec.get_model()
 docs = doc2vec.get_docs()
-print(docs[13])
-# model.docvecs.most_similar(docs[13])
 
+doc_id = np.random.randint(model.docvecs.count)  # Pick random doc; re-run cell for more examples
+doc_id = [i for i, x in enumerate(docs) if x[1][0] == 'Sieve-of-Eratosthenes'][0]
 
-
-
-
+inferred_docvec = model.infer_vector(docs[doc_id].words)
+print('\n%s:' % (docs[doc_id].tags[0]))
+for vec in model.docvecs.most_similar([inferred_docvec], topn=model.docvecs.count):
+    print('%s' % str(vec))
